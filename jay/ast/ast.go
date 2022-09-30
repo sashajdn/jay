8f6a8022aa8e182406e7fcad6db2454eba34f2bb
sasha <alexperkins.dev@gmail.com>
2022-09-30 21:56:16 +0100

    chore(parser): extend parser to parse return statements

--- jay/ast/ast.go
+++ jay/ast/ast.go
@@ -38,6 +38,14 @@ type LetStatement struct {
 func (l *LetStatement) statementNode()       {}
 func (l *LetStatement) TokenLiteral() string { return l.Token.Literal }
 
+type ReturnStatement struct {
+	Token       token.Token // the token.RETURN
+	ReturnValue Expression
+}
+
+func (r *ReturnStatement) statementNode()       {}
+func (r *ReturnStatement) TokenLiteral() string { return r.Token.Literal }
+
 type Identifier struct {
 	Token token.Token
 	Value string
